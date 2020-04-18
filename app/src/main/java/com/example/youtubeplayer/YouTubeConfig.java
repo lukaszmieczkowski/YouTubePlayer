f7a682b8bb7dc148fe55f0ffd376d0c48d0540fb
lukaszmieczkowski <lukaszmieczkowski.kontakt@gmail.com>
2020-04-18 13:01:29 +0200

    Youtube Player API

--- app/src/main/java/com/example/youtubeplayer/YouTubeConfig.java
+++ app/src/main/java/com/example/youtubeplayer/YouTubeConfig.java
@@ -3,7 +3,7 @@ package com.example.youtubeplayer;
 public class YouTubeConfig {
     public YouTubeConfig() {
     }
-    private static final String API_KEY = "AIzaSyD5Wf2-x_n2Hiw4YlCXrnVpYajq6X-aszw";
+    private static final String API_KEY = "TUTAJ WRZUCIC API KEY ";
 
     public static String getApiKey() {
         return API_KEY;
